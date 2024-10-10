ad6dfc71d9cd5a578e8a761102d34ea799b36feb
Wongani Chulu <wonganichulu@gmail.com>
2024-10-10 17:37:12 +0200

    Added 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type annotated function returns the string of a float"""
+
+
+def to_str(n: float) -> str:
+    """Returns string representation of a given float"""
+    return str(n)
