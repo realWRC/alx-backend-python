79df363af589b5e3397d27a769d416b5d46105ad
Wongani Chulu <wonganichulu@gmail.com>
2024-10-10 17:54:04 +0200

    Added 5-sum_list.py fix

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,9 +5,5 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """Returns string representation of a given float"""
-    total: float = 0.0
-    for i in range(0, len(input_list) - 1):
-        total += input_list[i]
-
-    return total
+    """Sums values in a list of floats"""
+    return float(sum(input_list))
