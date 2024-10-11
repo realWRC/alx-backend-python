1a8dbd96eb1b4a8c741f1cb77ba8f8093cd150f9
Wongani Chulu <wonganichulu@gmail.com>
2024-10-11 03:04:35 +0200

    Made 100-safe_first_element.py executable and added 101-safely_get_value.py

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Duck-typed annotations fix"""
+
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping[Any, Any], key: Any, default:
+                     Union[T, None] = None) -> Union[Any, T]:
+    """Function that does the thing"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
