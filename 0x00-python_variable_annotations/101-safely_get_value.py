377ffc9679e26617eae1474a9cb50f33cc9b3979
Wongani Chulu <wonganichulu@gmail.com>
2024-10-11 03:14:34 +0200

    Added 101-safely_get_value.py fix

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,19 +1,46 @@
 #!/usr/bin/env python3
+# """
+# Given the parameters and the return values, add
+# type annotations to the function
+# """
+#
+#
+# from typing import Mapping, Any, Union, TypeVar
+#
+#
+# T = TypeVar('T')
+#
+#
+# def safely_get_value(dct: Mapping[Any, Any], key: Any, default:
+#                      Union[T, None] = None) -> Union[Any, T]:
+#     """Type annotations for the function"""
+#     if key in dct:
+#         return dct[key]
+#     else:
+#         return default
+"""Given the parameters and the return values, add type
+annotations to the function
+
+Hint: look into TypeVar
+
+def safely_get_value(dct, key, default = None):
+    if key in dct:
+        return dct[key]
+    else:
+        return default
 """
-Given the parameters and the return values, add
-type annotations to the function
-"""
 
 
-from typing import Mapping, Any, Union, TypeVar
+import typing
 
 
-T = TypeVar('T')
+T = typing.TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[Any, Any], key: Any, default:
-                     Union[T, None] = None) -> Union[Any, T]:
-    """Type annotations for the function"""
+def safely_get_value(dct: typing.Mapping, key: typing.Any, default:
+                     typing.Union[T, None] = None) -> \
+        typing.Union[typing.Any, T]:
+    """annotations of the function"""
     if key in dct:
         return dct[key]
     else:
