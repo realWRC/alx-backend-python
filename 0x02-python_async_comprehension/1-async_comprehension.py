47879cc20f41f9e5f913c968da23a15a04f42b1e
Wongani Chulu <wonganichulu@gmail.com>
2024-10-15 19:51:54 +0200

    Added 1-async_comprehension.py

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+A Coroutine function that takes no arguments.
+"""
+
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """
+    The coroutine will loop 10 times, each time asynchronously wait 1 second,
+    then yield a random number between 0 and 10. Use the random module.
+    """
+    return[i async for i in async_generator()]
