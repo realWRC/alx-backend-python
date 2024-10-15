489b285fa64a31c1b73699fd5212353e99bcf86a
Wongani Chulu <wonganichulu@gmail.com>
2024-10-15 19:57:49 +0200

    Added 1-async_comprehension.py fix

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,11 @@
 A Coroutine function that takes no arguments.
 """
 
-import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     The coroutine will loop 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10. Use the random module.
