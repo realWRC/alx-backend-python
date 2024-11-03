d38eeea69ec1b6610ecc9b85f06dc562592cda7a
Wongani Chulu <wonganichulu@gmail.com>
2024-11-03 18:37:55 +0200

    Added solutions for tasks 0 - 2

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Unittests for python code implimemnted in directory
+"""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from utils import access_nested_map, get_json
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Tests the access_nested_map function"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Tests return values"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+    
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Tests for KeyError"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), repr(path[-1]))
+    
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_get.return_value.json.return_value = test_payload
+        result = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
